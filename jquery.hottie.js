4566a45f9d0d68440acae899370a0df822646c96
David Larsen <david.larsen@gmail.com>
2011-08-10 08:17:04 -0700

    Improved null handling

--- jquery.hottie.js
+++ jquery.hottie.js
@@ -1,30 +1,35 @@
 
 ;(function($) {
 
-    $.fn.hottie = function(options) {
+  $.fn.hottie = function(options) {
+
+    var plugin = $(this);
     
-      var plugin = $(this);
+    var init = function() {
+      var max = settings.readValue($(plugin[0]));
+      var min = settings.readValue($(plugin[0]));
       
-      var init = function() {
-        var max = settings.readValue($(plugin[0]));
-        var min = settings.readValue($(plugin[0]));
-        
-        plugin.each(function() {
-          var val = settings.readValue($(this));
+      plugin.each(function() {
+        var val = settings.readValue($(this));
+        if (val != null) {
           min = Math.min( min, val );
           max = Math.max( max, val );
-          $(this).data('val', val);
-        });
+        }
+        $(this).data('val', val);
+      });
 
-        var range = max - min;
-        plugin.each(function() {
-          var val = $(this).data('val');
+      var range = max - min;
+      plugin.each(function() {
+        var val = $(this).data('val');
+        var c = nullColor;
+        if (val != null) {
           var adj = val - min;
           var pct = 1.0 * adj / range;
-          var c = getColor(pct);
-          $(this).css('background-color', c);
-        });
-      };
+          c = getColor(pct);
+        }
+        $(this).css('background-color', c);
+      });
+    };
 
     var settings = {
       readValue : function(e) {
@@ -32,65 +37,63 @@
       }
     };
 
-      if ( options ) { 
-        $.extend( settings, options );
+    if ( options ) { 
+      $.extend( settings, options );
+    }
+
+    function hex2num(hex) {
+      if(hex.charAt(0) == "#") hex = hex.slice(1); //Remove the '#' char - if there is one.
+      var triplet = new Array();
+      var int1,int2;
+      for(var i=0;i<6;i+=2) {
+          var part = hex.charAt(i) + hex.charAt(i+1); 
+          triplet.push(parseInt(part,16));
       }
+      return triplet;
+    }
 
-
-
-      function hex2num(hex) {
-        if(hex.charAt(0) == "#") hex = hex.slice(1); //Remove the '#' char - if there is one.
-        var triplet = new Array();
-        var int1,int2;
-        for(var i=0;i<6;i+=2) {
-            var part = hex.charAt(i) + hex.charAt(i+1); 
-            triplet.push(parseInt(part,16));
-        }
-        return triplet;
-      }
-
-      function num2hex(triplet) {
-        var result = "#";
-        for(var i=0;i<3;i++) {
-            var hex = Math.round(triplet[i]).toString(16);
-            while (hex.length < 2) {
-              hex = "0" + hex;
-            }
-            result += hex; 
-        }
-        return result;
-      }
-
-  function rgbToHsv(triplet){
-      var r, g, b;
-      r = triplet[0];
-      g = triplet[1];
-      b = triplet[2];
-
-      r = r/255, g = g/255, b = b/255;
-      var max = Math.max(r, g, b), min = Math.min(r, g, b);
-      var h, s, v = max;
-
-      var d = max - min;
-      s = max == 0 ? 0 : d / max;
-
-      if(max == min){
-          h = 0; // achromatic
-      }else{
-          switch(max){
-              case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-              case g: h = (b - r) / d + 2; break;
-              case b: h = (r - g) / d + 4; break;
+    function num2hex(triplet) {
+      var result = "#";
+      for(var i=0;i<3;i++) {
+          var hex = Math.round(triplet[i]).toString(16);
+          while (hex.length < 2) {
+            hex = "0" + hex;
           }
-          h /= 6;
+          result += hex; 
       }
-      h = Math.max(0, Math.min(h, 1));
-      s = Math.max(0, Math.min(s, 1));
-      v = Math.max(0, Math.min(v, 1));
-      return [h, s, v];
-  }
+      return result;
+    }
 
-  function hsvToRgb(triplet){
+    function rgbToHsv(triplet){
+        var r, g, b;
+        r = triplet[0];
+        g = triplet[1];
+        b = triplet[2];
+
+        r = r/255, g = g/255, b = b/255;
+        var max = Math.max(r, g, b), min = Math.min(r, g, b);
+        var h, s, v = max;
+
+        var d = max - min;
+        s = max == 0 ? 0 : d / max;
+
+        if(max == min){
+            h = 0; // achromatic
+        }else{
+            switch(max){
+                case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+                case g: h = (b - r) / d + 2; break;
+                case b: h = (r - g) / d + 4; break;
+            }
+            h /= 6;
+        }
+        h = Math.max(0, Math.min(h, 1));
+        s = Math.max(0, Math.min(s, 1));
+        v = Math.max(0, Math.min(v, 1));
+        return [h, s, v];
+    }
+
+    function hsvToRgb(triplet){
       var h = triplet[0];
       var s = triplet[1];
       var v = triplet[2];
@@ -119,33 +122,33 @@
 
       var getColor = function(percent){
         
-        if (percent == null)
-          return nullColor;
+      if (percent == null)
+        return nullColor;
 
-        var colors = colorArray.length;
+      var colors = colorArray.length;
 
-          var colorPosition = (percent * (colors - 1));
-          var sIndex = Math.floor(colorPosition);
-          sIndex = Math.min(colors - 2, sIndex);
+      var colorPosition = (percent * (colors - 1));
+      var sIndex = Math.floor(colorPosition);
+      sIndex = Math.min(colors - 2, sIndex);
 
-          var s = colorArray[sIndex];
-          var e = colorArray[sIndex+1];
+      var s = colorArray[sIndex];
+      var e = colorArray[sIndex+1];
 
-          var sHSL = rgbToHsv(hex2num(s));
-          var eHSL = rgbToHsv(hex2num(e));
+      var sHSL = rgbToHsv(hex2num(s));
+      var eHSL = rgbToHsv(hex2num(e));
 
-          var interiorPercent = (percent * (colors - 1)) - sIndex;
+      var interiorPercent = (percent * (colors - 1)) - sIndex;
 
-          var dispRGB = hsvToRgb(transition3(interiorPercent, 1, sHSL, eHSL));
-          return num2hex(dispRGB);
-      };
+      var dispRGB = hsvToRgb(transition3(interiorPercent, 1, sHSL, eHSL));
+      return num2hex(dispRGB);
+    };
 
-  function transition(value, maximum, start_point, end_point){
+    function transition(value, maximum, start_point, end_point){
       var r = start_point + (end_point - start_point)*value/maximum
       return r;
-      }
+    }
 
-  function transition3(value, maximum, s, e) {
+    function transition3(value, maximum, s, e) {
       var p = value / maximum;
       // hue has to wrap correctly around zero
 
@@ -157,7 +160,7 @@
       var saturation= transition(value, maximum, s[1], e[1]); // s
       var value= transition(value, maximum, s[2], e[2]); // v
       return [hue, saturation, value];
-  }
+    }
 
       // min to max, evenly spaced
       var colorArray = [ 
@@ -171,11 +174,15 @@
         "#fda659",
         "#ff4a53"
       ];
-      
+
       var nullColor = "#333333";
-      
+
       init();
-      
+
     }
 
 })(jQuery);
+
+
+
+
